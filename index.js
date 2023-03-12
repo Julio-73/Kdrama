c76769cb66d0e9eeb577232692a8f548c7fe36cc
julio-cesar <juliocesarquispegarrido@gmail.com>
2023-03-11 21:17:52 -0500

    Cambios

---
+++ index.js
@@ -0,0 +1,16 @@
+/*========Scrolll========*/
+
+ScrollReveal({
+  reset: true,
+  distance: "50 px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".contenedor h1", { origin: "left" });
+
+ScrollReveal().reveal("p", { origin: "right" });
+
+ScrollReveal().reveal("h2", { origin: "left" });
+
+ScrollReveal().reveal("li", { origin: "right" });
